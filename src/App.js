b83e3e22fd983487b9861248a287d5e05193bb91
Renaud <yakuno@Renauds-MacBook-Pro.local>
2020-09-15 14:56:37 +0900

    exagerated timing transition for poc

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
         <CSSTransition
           key={location.key}
           classNames="page"
-          timeout={800}
+          timeout={1500}
         >
           <div className="page">
             <Switch location={location}>
